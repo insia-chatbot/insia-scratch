8e98238020c1bd4e0cdbdc2964e736a5e8329bac
Bongibault Romain <bongibault.romain@gmail.com>
2025-02-24 10:37:27 +0100

    changes

--- insia_scratch/train.py
+++ insia_scratch/train.py
@@ -8,16 +8,18 @@ def train_model(model, train_loader, num_epochs=10, learning_rate=0.001, save=No
 
     for epoch in range(num_epochs):
         total_loss = 0
+
         for x_batch, y_batch in train_loader:
+            # evaluate the loss
             print(x_batch, y_batch)
             optimizer.zero_grad()
-            output = model(x_batch)
+            logits, output = model(x_batch)
             loss = F.cross_entropy(output.view(-1, len(model.idx_to_char)), y_batch.view(-1))
             loss.backward()
             optimizer.step()
             total_loss += loss.item()
 
-        if save:
-            torch.save(model.state_dict(), save)
+            if save:
+                torch.save(model.state_dict(), save)
 
-        print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {total_loss/len(train_loader)}")
+            print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss}")
